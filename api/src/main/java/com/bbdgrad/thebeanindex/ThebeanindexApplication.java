4e7097ac898e9b84985a4eed07f78c3438d51c8d
motha003 <avishkar.motheelal11@gmail.com>
2024-03-04 20:15:48 +0200

    Initial Commit

---
+++ api/src/main/java/com/bbdgrad/thebeanindex/ThebeanindexApplication.java
@@ -0,0 +1,13 @@
+package com.bbdgrad.thebeanindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebeanindexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThebeanindexApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/bbdgrad/thebeanindex/ThebeanindexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbdgrad.thebeanindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebeanindexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
