938f76b3b6b888cd68e078bf9882b5b8b3f3eda6
Avi <avishkar.motheelal11@gmail.com>
2024-03-06 22:43:57 +0200

    Add deployment role and infrastructure deploy workflow

--- iac/src/main/java/com/myorg/IacStack.java
+++ iac/src/main/java/com/myorg/IacStack.java
@@ -1,44 +1,27 @@
 package com.myorg;
 
-import java.util.List;
-import java.util.Map;
-
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.docdb.DatabaseSecret;
-import software.amazon.awscdk.services.ec2.IIpAddresses;
-import software.amazon.awscdk.services.ec2.IPeer;
-import software.amazon.awscdk.services.ec2.Instance;
-import software.amazon.awscdk.services.ec2.InstanceClass;
-import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ec2.IpAddresses;
-import software.amazon.awscdk.services.ec2.Peer;
-import software.amazon.awscdk.services.ec2.Port;
-import software.amazon.awscdk.services.ec2.SecurityGroup;
-import software.amazon.awscdk.services.ec2.SubnetConfiguration;
-import software.amazon.awscdk.services.ec2.SubnetSelection;
-import software.amazon.awscdk.services.ec2.SubnetType;
-import software.amazon.awscdk.services.ec2.Vpc;
-import software.amazon.awscdk.services.rds.Credentials;
-import software.amazon.awscdk.services.rds.DatabaseInstance;
-import software.amazon.awscdk.services.rds.DatabaseInstanceEngine;
-import software.amazon.awscdk.services.rds.IInstanceEngine;
-import software.amazon.awscdk.services.rds.SqlServerEngineVersion;
-import software.amazon.awscdk.services.rds.SqlServerExInstanceEngineProps;
-import software.amazon.awscdk.services.secretsmanager.*;
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.iam.*;
+import software.amazon.awscdk.services.rds.*;
 import software.constructs.Construct;
+
+import java.util.List;
+import java.util.Map;
 
 public class IacStack extends Stack {
     public IacStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
 
+
     public IacStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
-
 
         IInstanceEngine engine = DatabaseInstanceEngine.sqlServerEx(SqlServerExInstanceEngineProps.builder()
             .version(SqlServerEngineVersion.VER_16)
@@ -48,11 +31,10 @@ public class IacStack extends Stack {
 
         final int PORT = 1433;
 
-
-        DatabaseSecret dbSuperUserSecret = DatabaseSecret.Builder.create(this,  "dbSuperUserSecret")
+        DatabaseSecret dbSuperUserSecret = DatabaseSecret.Builder.create(this, "dbSuperUserSecret")
             .username("DBSuperUser")
             .build();
-            
+
         Vpc dbVpc = Vpc.Builder.create(this, "dbVpc")
             .ipAddresses(IpAddresses.cidr("10.0.0.0/16"))
             .natGateways(1)
@@ -71,7 +53,7 @@ public class IacStack extends Stack {
 
         databaseSecurityGroup.addEgressRule(Peer.anyIpv4(), Port.tcp(PORT));
         databaseSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(PORT));
-        
+
         DatabaseInstance databaseInstance = DatabaseInstance.Builder.create(this, "the-bean-index-instance")
             .vpc(dbVpc)
             .vpcSubnets(SubnetSelection.builder()
@@ -80,10 +62,42 @@ public class IacStack extends Stack {
             .instanceType(instanceType)
             .engine(engine)
             .securityGroups(List.of(databaseSecurityGroup))
-            .credentials(Credentials.fromSecret(dbSuperUserSecret)) 
+            .credentials(Credentials.fromSecret(dbSuperUserSecret))
             .backupRetention(Duration.days(0))
             .removalPolicy(RemovalPolicy.DESTROY)
             .build();
 
+        // GitHub Deploy Role
+        String gitHubUrl = "token.actions.githubusercontent.com";
+        OpenIdConnectProvider githubProvider = OpenIdConnectProvider.Builder.create(this, "githubProvider")
+            .url("https://" + gitHubUrl)
+            .clientIds(List.of("sts.amazonaws.com"))
+            .build();
+
+        Map<String, Object> conditions = Map.of("StringLike", Map.of(gitHubUrl + ":sub", "repo:The-Bean-Index/TheBeanIndex:*"));
+
+        PolicyStatement assumeRoleStatement = PolicyStatement.Builder.create()
+            .actions(List.of("sts:AssumeRole"))
+            .effect(Effect.ALLOW)
+            .resources(List.of("arn:aws:iam::*:role/cdk-*"))
+            .build();
+
+        PolicyStatement getSecretsStatement = PolicyStatement.Builder.create()
+            .actions(List.of("secretsmanager:GetSecretValue"))
+            .effect(Effect.ALLOW)
+            .resources(List.of("*"))
+            .build();
+
+        PolicyDocument policyDocument = PolicyDocument.Builder.create()
+            .statements(List.of(assumeRoleStatement, getSecretsStatement))
+            .build();
+
+        Role githubDeployRole = Role.Builder.create(this, "githubDeployRole")
+            .roleName("GitHubDeployRole")
+            .description("This role is used via GitHub Actions to deploy with AWS CDK and access stored secrets")
+            .assumedBy(new OpenIdConnectPrincipal(githubProvider, conditions))
+            .inlinePolicies(Map.of("deploymentPolicies", policyDocument))
+            .maxSessionDuration(Duration.hours(1))
+            .build();
     }
 }
