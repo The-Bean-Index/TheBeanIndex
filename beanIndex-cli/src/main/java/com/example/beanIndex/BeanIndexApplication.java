b28aa023338f48a506da799236738292283c26c2
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-03-07 17:58:48 +0200

    initial creation of spring application for cli

---
+++ beanIndex-cli/src/main/java/com/example/beanIndex/BeanIndexApplication.java
@@ -0,0 +1,13 @@
+package com.example.beanIndex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanIndexApplication.class, args);
+	}
+
+}
