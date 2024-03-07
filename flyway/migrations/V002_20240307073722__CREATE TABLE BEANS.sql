17b13369bbd2c2ee002f0ea07280adc176dfbe53
tawanda-bbd <156799248+tawanda-bbd@users.noreply.github.com>
2024-03-07 10:26:29 +0200

    Flyway Addition

---
+++ flyway/migrations/V002_20240307073722__CREATE TABLE BEANS.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [beans] (
+  [id] int PRIMARY KEY,
+  [name] nvarchar(255),
+  [beanPrice] money
+)
+GO
---
+++ flyway/migrations/V004_20240307073836__CREATE TABLE GDP.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [GDP] (
+  [id] int PRIMARY KEY,
+  [country_id] int,
+  [year_id] int,
+  [gdp_amount] money
+)
+GO
