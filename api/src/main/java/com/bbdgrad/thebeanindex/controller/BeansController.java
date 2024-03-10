a60e5de86081c34f44e1bf84dbe413f05a9c65f8
ShaariqBBD <shaariq.patel@bbd.co.za>
2024-03-10 14:32:42 +0200

    Added BeansDTO, Updated BeansController

--- api/src/main/java/com/bbdgrad/thebeanindex/controller/BeansController.java
+++ api/src/main/java/com/bbdgrad/thebeanindex/controller/BeansController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bbdgrad.thebeanindex.repository.BeansRepository;
+import com.bbdgrad.thebeanindex.Dtos.BeansDto;
 import com.bbdgrad.thebeanindex.model.Beans;
 
 import java.util.List;
@@ -22,12 +23,18 @@ public class BeansController {
         this.beansRepository = beansRepository;
     }
 
-    @GetMapping("/names")
-    public List<String> getAllBeanNames() {
+    @GetMapping("/")
+    public List<BeansDto> getAllBeanNames() {
         List<Beans> beans = beansRepository.findAll();
+        return beans.stream().map(BeansDto::toDto).collect(Collectors.toList());
+    }
 
-        return beans.stream()
+    @GetMapping("/names")
+    public List<String> getAllBeanPrices() {
+        List<String> beanPrices = beansRepository.findAll().stream()
                 .map(Beans::getName)
+                .map(Object::toString)
                 .collect(Collectors.toList());
+        return beanPrices;
     }
 }
