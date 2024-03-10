1611b92bff3c9838ef0b3192f20fc94aa8e2c12d
ShaariqBBD <shaariq.patel@bbd.co.za>
2024-03-10 16:19:18 +0200

    Fixed output format

--- api/src/main/java/com/bbdgrad/thebeanindex/controller/BeansController.java
+++ api/src/main/java/com/bbdgrad/thebeanindex/controller/BeansController.java
@@ -8,10 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.bbdgrad.thebeanindex.repository.BeansRepository;
+import com.bbdgrad.thebeanindex.Dtos.BeanNamesDto;
 import com.bbdgrad.thebeanindex.Dtos.BeansDto;
+import com.bbdgrad.thebeanindex.Dtos.BeansResponseDto;
 import com.bbdgrad.thebeanindex.model.Beans;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -25,19 +29,22 @@ public class BeansController {
         this.beansRepository = beansRepository;
     }
 
-    @GetMapping("/")
-    public List<BeansDto> getAllBeanNames() {
+    @GetMapping("/all")
+    public BeansResponseDto getAllBeans() {
         List<Beans> beans = beansRepository.findAll();
-        return beans.stream().map(BeansDto::toDto).collect(Collectors.toList());
+        List<BeansDto> beansDtoList = beans.stream().map(BeansDto::toDto).collect(Collectors.toList());
+        return new BeansResponseDto(beansDtoList);
     }
 
     @GetMapping("/names")
-    public ResponseEntity<List<String>> getAllBeanPrices() {
+    public ResponseEntity<BeanNamesDto> getAllBeanNames() {
         List<String> beanNames = beansRepository.findAll().stream()
                 .map(Beans::getName)
-                .map(Object::toString)  
+                .map(Object::toString)
                 .collect(Collectors.toList());
-        
-        return new ResponseEntity<>(beanNames, HttpStatus.OK);
+
+        BeanNamesDto beanNamesDto = new BeanNamesDto(beanNames);
+
+        return new ResponseEntity<>(beanNamesDto, HttpStatus.OK);
     }
 }
