052adb5103eb9a5d70306c537c596a7e9cae0a97
ShaariqBBD <shaariq.patel@bbd.co.za>
2024-03-11 19:19:43 +0200

    Updated getters and setters

--- api/src/main/java/com/bbdgrad/thebeanindex/Dtos/BeanNamesDto.java
+++ api/src/main/java/com/bbdgrad/thebeanindex/Dtos/BeanNamesDto.java
@@ -1,19 +1,14 @@
 package com.bbdgrad.thebeanindex.Dtos;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class BeanNamesDto {
     private List<String> beanNames;
 
     public BeanNamesDto(List<String> beanNames) {
-        this.beanNames = beanNames;
-    }
-
-    public List<String> getBeanNames() {
-        return beanNames;
-    }
-
-    public void setBeanNames(List<String> beanNames) {
         this.beanNames = beanNames;
     }
 }
--- api/src/main/java/com/bbdgrad/thebeanindex/Dtos/CountriesResponseDto.java
+++ api/src/main/java/com/bbdgrad/thebeanindex/Dtos/CountriesResponseDto.java
@@ -1,19 +1,14 @@
 package com.bbdgrad.thebeanindex.Dtos;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class CountriesResponseDto {
     private List<CountriesDto> Countries;
 
     public CountriesResponseDto(List<CountriesDto> countries) {
-        this.Countries = countries;
-    }
-
-    public List<CountriesDto> getCountries() {
-        return Countries;
-    }
-
-    public void setCountries(List<CountriesDto> countries) {
         this.Countries = countries;
     }
 }
